7236e85130c2d2a2bd8c02baa1303ea442f50268
david <godicked@gmail.com>
2024-10-07 23:29:46 +0200

    update scolo sounds

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -12,6 +12,6 @@ var songs: Dictionary = {
 
 var musics = {
 	"shroom": preload("res://sounds/Son-ouverture champignon.mp3"),
-	"scolo": preload("res://sounds/son-scolopandre.mp3"),
+	"scolo": preload("res://sounds/Scolopandre_V2.mp3"),
 	"teleport": preload("res://sounds/Musique-Teleportation.mp3")
 }
