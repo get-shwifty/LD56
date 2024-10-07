7d6ae0cf4cc328af6d9e9698fb86010d3a9e1605
david <godicked@gmail.com>
2024-10-08 01:50:47 +0200

    fix 2

--- start_screen.gd
+++ start_screen.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
+var started = false
+
 func _process(delta):
+	if started:
+		return
 	if Input.is_anything_pressed():
-		print('change scene')
-		get_tree().change_scene_to_file("res://main_edouard.tscn")
+		started = true
+		#print('change scene')
+		#get_tree().change_scene_to_file("res://main_edouard.tscn")
+		$AnimationPlayer.play("fade")
