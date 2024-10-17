8e2e18f746ac24584d95a893ce50ff620ec720d6
david <godicked@gmail.com>
2024-10-17 16:14:58 +0200

    save fix for light scaling + adjustements on map

--- src/world/cave_manager.gd
+++ src/world/cave_manager.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 class_name CaveManager
 
+@export var on: bool = false
 @export var cover: Node2D = null
 @onready var lights: Node2D = $Overlay/CanvasGroup/Lights
 
@@ -11,6 +12,9 @@ func _ready():
 	$Overlay/CanvasGroup/Dark.show()
 	set_ratio(0)
 	cover.get_node("DarkLayer").show()
+	
+	if on:
+		set_ratio(1.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +23,7 @@ func _process(delta):
 
 func set_ratio(ratio: float):
 	set_shader_value(ratio)
-	cover.modulate = Color(Color.WHITE, 1.0 - pow(ratio, 4))
+	cover.modulate = Color(Color.WHITE, 1.0 - pow(ratio, 2))
 
 
 func set_shader_value(value: float):
