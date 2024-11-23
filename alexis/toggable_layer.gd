650bed04df74a197f1b5ef48ac968c5a8f61f4ca
Alexis Schad <schad.alexis@gmail.com>
2024-11-24 00:50:58 +0100

    few things for the playtest

---
+++ alexis/toggable_layer.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+func _ready():
+	show()
+	for area: Area2D in find_children("*", "Area2D", false):
+		area.collision_layer = 0
+		area.collision_mask = 128
+		area.body_entered.connect(_on_area_2d_body_entered)
+		area.body_exited.connect(_on_area_2d_body_exited)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	show()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	hide()
