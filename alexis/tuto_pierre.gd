1d6be91233ce58a6c453cd8a4aa6ff6ce2a947ba
Alexis Schad <schad.alexis@gmail.com>
2024-11-24 20:58:14 +0100

    tuto champ + pierre

---
+++ alexis/tuto_pierre.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func on_song(song: String):
+	if song.ends_with("bca"):
+		$RuneB.activate()
+		$RuneC.activate()
+		$RuneA.activate()
+	elif song.ends_with("bc"):
+		$RuneB.activate()
+		$RuneC.activate()
+		$RuneA.deactivate()
+	elif song.ends_with("b"):
+		$RuneB.activate()
+		$RuneC.deactivate()
+		$RuneA.deactivate()
+	else:
+		$RuneB.deactivate()
+		$RuneC.deactivate()
+		$RuneA.deactivate()
