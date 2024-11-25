2fb0e35a0d961a9ec775116d4bd15d7bbc8d1c4d
Tyrannas <someonefefe@gmail.com>
2024-11-25 15:53:39 +0100

    new tuto + secret

---
+++ alexis/tuto_perso.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+func on_song(song: String):
+	if song == "dcab":
+		$RuneD.activate()
+		$RuneC.activate()
+		$RuneA.activate()
+		$RuneB.activate()
+	elif song == "dca":
+		$RuneD.activate()
+		$RuneC.activate()
+		$RuneA.activate()
+		$RuneB.deactivate()
+	elif song == "dc":
+		$RuneD.activate()
+		$RuneC.activate()
+		$RuneA.deactivate()
+		$RuneB.deactivate()
+	elif song == "d":
+		$RuneD.activate()
+		$RuneC.deactivate()
+		$RuneA.deactivate()
+		$RuneB.deactivate()
+	else:
+		$RuneD.deactivate()
+		$RuneC.deactivate()
+		$RuneA.deactivate()
+		$RuneB.deactivate()
