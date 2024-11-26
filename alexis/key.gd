e6cb509b28c0cc004dad4847b0f4fb53971e333b
Tyrannas <someonefefe@gmail.com>
2024-11-26 10:25:03 +0100

    key sound

--- alexis/key.gd
+++ alexis/key.gd
@@ -7,4 +7,9 @@ func _ready():
 	$AnimatedSprite2D.play()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	visible = false
+	$AudioStreamPlayer2D.play()
+
+
+func _on_audio_stream_player_2d_finished() -> void:
 	on_collected.emit()
