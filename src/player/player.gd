2e3692e7e2c061dc3d4f254b8a22b4264e6c974a
Alexis Schad <schad.alexis@gmail.com>
2024-11-26 20:16:11 +0100

    better better dash

--- src/player/player.gd
+++ src/player/player.gd
@@ -157,7 +157,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right") if can_input else 0.0
 	if state == DASH:
 		velocity.y = 0.0
-		velocity.x = dash_dir * RUN_SPEED * lerp(0.0, 1.5, dash_time*2)
+		velocity.x = dash_dir * RUN_SPEED * lerp(0.0, 2.2, (dash_time*2)**2)
 	elif direction:
 		%AnimatedSprite2D.scale.x = sign(direction)
 		%PlayerMask.scale.x = sign(direction)
